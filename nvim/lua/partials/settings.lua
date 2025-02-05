ce075c6bfdedfe28fdd41f5d9806edb970b08d87
Luke Gibbard <gibbard27@gmail.com>
2025-02-05 11:43:11 +0000

    nvim: set colorcolumn

--- nvim/lua/partials/settings.lua
+++ nvim/lua/partials/settings.lua
@@ -2,3 +2,11 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.colorcolumn = "100" -- ColorColumn width
+
+vim.api.nvim_exec(
+	[[
+      hi ColorColumn guibg=#665c54
+    ]],
+	false
+)
