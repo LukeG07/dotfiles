062d15f7236a26c6b17d883e907359546d8749ce
Luke Gibbard <gibbard27@gmail.com>
2025-04-04 17:06:09 +0100

    add quickfix nav hotkeys

--- nvim/lua/partials/settings.lua
+++ nvim/lua/partials/settings.lua
@@ -96,6 +96,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Cycle through quickfix list items
+vim.keymap.set("n", "]e", "<Cmd>try | cnext | catch | cfirst | catch | endtry<CR>")
+vim.keymap.set("n", "[e", "<Cmd>try | cprevious | catch | clast | catch | endtry<CR>")
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
